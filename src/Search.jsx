0d88b63f81236a13acdfe448825491b274c6b80d
Sudip-khadka <ksudip0123@gmail.com>
2024-08-28 14:58:23 +0545

    added github url

--- src/Search.jsx
+++ src/Search.jsx
@@ -43,6 +43,7 @@ function Search() {
     <>
     <h2 className='headers'>Task 2: Implement and Test a React Component with API Integration (Take-Home)
     </h2>
+    <p>Github:- <a href='https://github.com/Sudip-khadka/searching-and-api-testing-with-jest'>View Code</a></p>
     <div className="search-container">
       <div className="search-bar">
         <input
